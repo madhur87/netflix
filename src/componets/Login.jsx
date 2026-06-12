a31c7b011d43f7737f2e5146cc80379a0200764e
Madhur Rastogi <madhur.rastogi@primussoft.com>
2026-06-12 14:20:06 +0530

    Build form signin/up

--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -1,16 +1,51 @@
 import Header from "./Header";
 import { BG_URL } from "../utils/constain";
+import { useState } from "react";
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignUpForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
     <div>
       <Header />
       <div className="absolute">
         <img src={BG_URL} alt="bg" />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-24">
-        <input type="text" placeholder="Email" className="p-2 m-2" />
-        <input type="password" placeholder="Password" className="p-2 m-2" />
-        <button className="p-4 m-4">Sign In</button>
+      <form className="w-3/12 absolute p-12 bg-black/70 my-36 mx-auto right-0 left-0 text-white rounded-lg">
+        <h1 className="font-bold text-3xl py-2">{isSignInForm ? "Sign In": "Sign Up"}</h1>
+        <input
+          type="text"
+          placeholder="Email or mobile number"
+          className="p-2 my-4 rounded bg-gray-800 border border-gray-600 w-full"
+        />
+
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-4 rounded bg-gray-800 border border-gray-600 w-full"
+          />
+        )}
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 my-4 rounded bg-gray-800 border border-gray-500 w-full"
+        />
+
+        <button className="bg-red-600 p-2 my-4 w-full rounded font-semibold">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4">
+          {isSignInForm ? "New to Netflix?" : "Already Register?"}
+          <a className="cursor-pointer" onClick={toggleSignUpForm}>
+            {isSignInForm ? " Sign Up " : " Sign In "}
+          </a>
+          Now
+        </p>
       </form>
     </div>
   );
